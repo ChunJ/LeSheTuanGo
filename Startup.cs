46249e420478be45db00571d47794047fded88d1
ChunJ <jester98741@hotmail.com>
2021-03-18 15:39:45 +0800

    add db context in startup

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using LeSheTuanGo.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,6 +21,7 @@ namespace LeSheTuanGo {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllersWithViews();
+            services.AddScoped<MidtermContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
