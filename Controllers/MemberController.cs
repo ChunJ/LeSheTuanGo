00b20a716ddd40ca20988601df2b3e77fba2159f
Jerry7572 <zxc0321520@gmail.com>
2021-03-18 16:30:05 +0800

    Member相關
    新增MemberController
    一些View

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeSheTuanGo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeSheTuanGo.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(MemberViewModel memberData)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ ViewModels/MemberViewModel.cs
@@ -0,0 +1,74 @@
+﻿using LeSheTuanGo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeSheTuanGo.ViewModels
+{
+    public class MemberViewModel
+    {
+        private Member iv_Member = null;
+        private Member p { get { return iv_Member; } }
+        public MemberViewModel()
+        {
+            iv_Member = new Member();
+        }
+        public MemberViewModel(Member p)
+        {
+            iv_Member = p;
+        }
+
+        //public Member()
+        //{
+        //    GarbageServiceOffers = new HashSet<GarbageServiceOffer>();
+        //    GarbageServiceUseRecords = new HashSet<GarbageServiceUseRecord>();
+        //    OrderBuyRecords = new HashSet<OrderBuyRecord>();
+        //    Orders = new HashSet<Order>();
+        //}
+
+        public int MemberId { get { return iv_Member.MemberId; } set { iv_Member.MemberId = value; } }
+
+        [DisplayName("姓")]
+        [Required(ErrorMessage = "不能為空")]
+        public string FirstName { get {return iv_Member.FirstName; } set {iv_Member.FirstName = value; } }
+
+        [DisplayName("名")]
+        [Required(ErrorMessage = "不能為空")]
+        public string LastName { get {return iv_Member.LastName; } set {iv_Member.LastName = value; } }
+
+        [DisplayName("電子信箱")]
+        [Required(ErrorMessage = "不能為空")]
+        public string Email { get {return iv_Member.Email; } set {iv_Member.Email = value; } }
+
+        [DisplayName("密碼")]
+        [Required(ErrorMessage = "不能為空")]
+        public string Password { get {return iv_Member.Password; } set {iv_Member.Password = value; } }
+
+        public string PasswordSalt { get {return iv_Member.PasswordSalt; } set {iv_Member.PasswordSalt = value; } }
+
+        public short DistrictId { get {return iv_Member.DistrictId; } set {iv_Member.DistrictId = value; } }
+
+        [DisplayName("地址")]
+        [Required(ErrorMessage = "不能為空")]
+        public string Address { get {return iv_Member.Address; } set { iv_Member.Address = value; } }
+
+        [DisplayName("緯度")]
+        public decimal Latitude { get { return iv_Member.Latitude; } set { iv_Member.Latitude = value; } }
+
+        [DisplayName("經度")]
+        public decimal Longitude { get { return iv_Member.Longitude; } set { iv_Member.Longitude = value; } }
+
+        [DisplayName("餘額")]
+        public int Balance { get { return iv_Member.Balance; } set { iv_Member.Balance = value; } }
+
+        public virtual DistrictRef District { get; set; }
+        public virtual ICollection<GarbageServiceOffer> GarbageServiceOffers { get; set; }
+        public virtual ICollection<GarbageServiceUseRecord> GarbageServiceUseRecords { get; set; }
+        public virtual ICollection<OrderBuyRecord> OrderBuyRecords { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+
+    }
+}
