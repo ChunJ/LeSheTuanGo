63ae271945266d5ca49f9a906f7b616f465e390c
ChunJ <jester98741@hotmail.com>
2021-03-22 05:16:19 +0800

    0322

--- Controllers/NewGroupOrderController.cs
+++ Controllers/NewGroupOrderController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using LeSheTuanGo.Models;
+using System.Text;
 
-namespace LeSheTuanGo.Controllers
-{
-    public class NewGroupOrderController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace LeSheTuanGo.Controllers {
+    public class NewGroupOrderController : Controller {
+        private readonly MidtermContext db;
+        public NewGroupOrderController(MidtermContext context) {
+            db = context;
+        }
+        public IActionResult Index() {
+            ViewData["Category"] = new SelectList(db.CategoryRefs, "CategoryId", "CategoryName");
+            ViewData["City"] = new SelectList(db.CityRefs, "CityId", "CityName");
             return View();
         }
     }
