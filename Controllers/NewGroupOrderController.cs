65b17f0dc15bf68d28ab826dd14cc0ba04c9499d
ChunJ <jester98741@hotmail.com>
2021-03-25 18:11:27 +0800

    3/25

--- Controllers/NewGroupOrderController.cs
+++ Controllers/NewGroupOrderController.cs
@@ -8,6 +8,7 @@ using LeSheTuanGo.Models;
 using System.Text;
 using Microsoft.AspNetCore.Http;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 // add session to use useid
 namespace LeSheTuanGo.Controllers {
@@ -24,11 +25,10 @@ namespace LeSheTuanGo.Controllers {
             //若有登入則使用使用者資訊，否則手動給預設值。
             if (HttpContext.Session.GetInt32(cUtility.Current_User_Id) != null) {
                 userId = HttpContext.Session.GetInt32(cUtility.Current_User_Id).Value;
-                Member user = db.Members.Where(m => m.MemberId == userId).First();
+                var user = db.Members.Where(m => m.MemberId == userId).Include(m=>m.District).First();
                 ViewData["Address"] = user.Address;
                 ViewData["DistrictId"] = user.DistrictId;
-                //ViewData["CityId"] = user.District.CityId;
-                ViewData["CityId"] = db.DistrictRefs.Where(d => d.DistrictId == user.DistrictId).First().CityId;
+                ViewData["CityId"] = user.District.CityId;
             } else {
                 ViewData["Address"] = "";
                 ViewData["DistrictId"] = 1;
@@ -38,6 +38,17 @@ namespace LeSheTuanGo.Controllers {
             ViewData["City"] = new SelectList(db.CityRefs, "CityId", "CityName");
             ViewData["GoRangeId"] = new SelectList(db.RangeRefs, "RangeId", "RangeInMeters");
             return View();
+        }
+
+        //see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public async Task<IActionResult> Index(Order order) {
+            order.EndTime = DateTime.Now;
+            order.StartTime = DateTime.Now;
+            Debug.WriteLine(order.EndTime);
+            //db.Add(order);
+            //await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
