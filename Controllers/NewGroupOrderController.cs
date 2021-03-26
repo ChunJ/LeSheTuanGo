1f2a7174391405c8dfa4dce09bceb0096b060dcc
ChunJ <jester98741@hotmail.com>
2021-03-26 16:45:42 +0800

    加入資料庫檔案
    在db資料夾裡面

--- Controllers/NewGroupOrderController.cs
+++ Controllers/NewGroupOrderController.cs
@@ -18,22 +18,20 @@ namespace LeSheTuanGo.Controllers {
             db = context;
         }
         public IActionResult Index() {
+            //this view is login user only
+            //if (HttpContext.Session.GetInt32(cUtility.Current_User_Id) == null{
+            //    return RedirectToAction("Login");
+            //}
             //暫時造假用，設定session裡的 user id
             HttpContext.Session.SetInt32(cUtility.Current_User_Id, 1);
 
-            int userId;
-            //若有登入則使用使用者資訊，否則手動給預設值。
-            if (HttpContext.Session.GetInt32(cUtility.Current_User_Id) != null) {
-                userId = HttpContext.Session.GetInt32(cUtility.Current_User_Id).Value;
-                var user = db.Members.Where(m => m.MemberId == userId).Include(m=>m.District).First();
-                ViewData["Address"] = user.Address;
-                ViewData["DistrictId"] = user.DistrictId;
-                ViewData["CityId"] = user.District.CityId;
-            } else {
-                ViewData["Address"] = "";
-                ViewData["DistrictId"] = 1;
-                ViewData["CityId"] = 1;
-            }
+            int userId = HttpContext.Session.GetInt32(cUtility.Current_User_Id).Value;
+            //使用使用者的地址
+            var user = db.Members.Where(m => m.MemberId == userId).Include(m=>m.District).First();
+            ViewData["Address"] = user.Address;
+            ViewData["DistrictId"] = user.DistrictId;
+            ViewData["CityId"] = user.District.CityId;
+
             ViewData["Category"] = new SelectList(db.CategoryRefs, "CategoryId", "CategoryName");
             ViewData["City"] = new SelectList(db.CityRefs, "CityId", "CityName");
             ViewData["GoRangeId"] = new SelectList(db.RangeRefs, "RangeId", "RangeInMeters");
@@ -43,11 +41,12 @@ namespace LeSheTuanGo.Controllers {
         //see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         public async Task<IActionResult> Index(Order order) {
-            order.EndTime = DateTime.Now;
+            order.HostMemberId = 1;
             order.StartTime = DateTime.Now;
             Debug.WriteLine(order.EndTime);
-            //db.Add(order);
-            //await db.SaveChangesAsync();
+            
+            db.Add(order);
+            await db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
     }
