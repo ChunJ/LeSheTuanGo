ad832b303875547536236bb46b329ddba37331b4
sayaotonathi <sayaotonathi@gmail.com>
2021-04-01 20:11:10 +0800

    修改連線字串

--- Models/MidtermContext.cs
+++ Models/MidtermContext.cs
@@ -39,8 +39,8 @@ namespace LeSheTuanGo.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Midterm;Integrated Security=True");
-                optionsBuilder.UseSqlServer("Server=tcp:msit129azureserver.database.windows.net,1433;Initial Catalog=Midterm;Persist Security Info=False;User ID=saya;Password=!QAZ2wsx;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Midterm;Integrated Security=True");
+                //optionsBuilder.UseSqlServer("Server=tcp:msit129azureserver.database.windows.net,1433;Initial Catalog=Midterm;Persist Security Info=False;User ID=saya;Password=!QAZ2wsx;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
             }
         }
