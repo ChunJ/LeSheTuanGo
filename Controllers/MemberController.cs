b7486525a2be6617b4c0e676bdb2fb8645f411c8
Jerry7572 <zxc0321520@gmail.com>
2021-04-09 09:44:15 +0800

    Revert "test"
    This reverts commit 18720248767ff65e174ab20b907d745a1e1d3296.

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -25,7 +25,6 @@ namespace LeSheTuanGo.Controllers
         {
             iv_host = host;
             db = context;
-            zczcz;
         }
 
         public IActionResult Login(string from = "Home/Index")
