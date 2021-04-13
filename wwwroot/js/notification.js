b8c822e8f20b9ce476122ac2baf89930edb1c108
sayaotonathi <sayaotonathi@gmail.com>
2021-04-13 17:43:45 +0800

    更新通知下拉選單功能

--- wwwroot/js/notification.js
+++ wwwroot/js/notification.js
@@ -3,7 +3,7 @@ $(function () {
     //接收事件
     connection.on("ReceiveNotification", function (groupName, groupType, orderId, message) {
         console.log(message)
-        $("#LayoutNotificationList").prepend(`<a class="dropdown - item" href="/ChatMessageRecords/Index?grouptype = ${groupType}& groupid = ${orderId}">${message}</a>`)
+        $("#LayoutNotificationList").prepend(`<a class="dropdown-item" href="/ChatMessageRecords/Index?grouptype=${groupType}&groupid=${orderId}">${message}</a>`)
     })
     //啟動時連線&依據登入ID加入group
     if (sessionMemberId != "") {
@@ -28,7 +28,6 @@ function createNotification(groupType, orderId, senderId, notifyContent) {
             var memberList = JSON.parse(data);
             for (let i = 0; i < memberList.length; i++) {
                 var group = "memberid_" + memberList[i];
-
                 connection.invoke("SendNotificationToGroup", group, groupType, orderId, notifyContent)
                     .catch(function (err) {
                         return console.error(err.toString());
